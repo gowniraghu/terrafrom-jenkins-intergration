dc3a666bce65c06aea80def46e116598debd969e
gowniraghu <48234482+gowniraghu@users.noreply.github.com>
2023-11-29 17:55:28 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,13 +1,13 @@
 variable "instance_type" {
     description = "type of the instance"
-    type = "string"
+    type = string
     default = "t2.micro"
   
 }
 
 variable "ami_id" {
     description = "id of the ec2 ami"
-    type = "string"
+    type = string
     default = "ami-0287a05f0ef0e9d9a"
   
 }
@@ -15,7 +15,7 @@ variable "ami_id" {
 
 variable "instance_name" {
     description = "name of the instance"
-    type = "string"
+    type = string
     default = "app-server"
     
   
